6673687363fd654e46d7165e35da76839f7d66c6
Harshit Prajapati <143312448+Harshitprajapati10@users.noreply.github.com>
2024-09-15 09:42:57 +0000

    added DRF

--- home/views.py
+++ home/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Book 
+from .serializers import BookSerializer
+
+
+class BookView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+BookViewAll = BookView.as_view() 
+
 
 def home(request):
     return HttpResponse("Hello, world!")
